a7e3241a243296b24b5d0abe4b7ece13f4c7039d
thetbsi <maytt4b567@gmail.com>
2016-01-13 20:37:25 -0500

    save

--- go/src/hello/main.go
+++ go/src/hello/main.go
@@ -3,8 +3,24 @@ package main
 import (
 	"fmt"
 	c "github.com/skilstak/go/colors"
+	i "github.com/skilstak/go/input"
 )
 
-func main() {
+func printMulti() {
 	fmt.Println(c.Clear + c.Multi("Hello World!"))
+}
+
+func printRandom() {
+	fmt.Println(c.Clear + c.Rc() + "Hello World!")
+}
+
+func main() {
+	//printMulti()
+	//printRandom()
+	a, thingsandstuff := i.Prompt("text")
+	if thingsandstuff != nil {
+		panic(thingsandstuff)
+
+	}
+	fmt.Println(a)
 }
