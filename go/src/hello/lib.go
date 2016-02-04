bf7ba16c351b4e8f780bd0d0c681bd3c828d99aa
thetbsi <maytt4b567@gmail.com>
2016-02-03 20:17:41 -0500

    save

--- go/src/hello/lib.go
+++ go/src/hello/lib.go
@@ -9,4 +9,12 @@ func PrintMulti(message string) {
 	fmt.Println(c.Clear + c.Multi(message) + c.X)
 }
 func PrintForever(message string) {
-	
+	for { 
+		fmt.Println(c.Clear + (message) + c.X)
+}
+func PrintPlain(message string) { 
+		fmt.Println("Hello" + (message) + c.X)
+}
+func PrintRandom(message string) {
+	fmt.Println(c.Clear + c.Rc() + (message) + c.X)
+}
